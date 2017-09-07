9af27147be5496d36e9cd3e848e03f603152f6c2
Ryan Feng <ryanfzyw@gmail.com>
2017-09-07 18:04:54 +1200

    added checks for duplicate firsts

--- src/com/ryanf/bnf/helpers/AstNodeHelper.java
+++ src/com/ryanf/bnf/helpers/AstNodeHelper.java
@@ -7,29 +7,78 @@ import com.ryanf.bnf.interfaces.IAstTree;
 import com.ryanf.bnf.types.AstNodeType;
 
 public class AstNodeHelper {
-	public static Vector<String> GetFirsts(IAstTree tree, IAstNode node) {
+	public static Vector<String> getFirsts(IAstTree tree, IAstNode node) throws Exception {
 		Vector<String> firsts = new Vector<String>();
 		if (tree != null && node != null) {
 			if (node.getType() == AstNodeType.ASIGN_STAT)
-				firsts.addAll(GetFirsts(tree, node.getChild(1)));
+				add(firsts, getFirsts(tree, node.getChild(1)));
 			else if (node.getType() == AstNodeType.ALTERNODELIST || node.getType() == AstNodeType.CHARLIST) {
 				for (int i = 0; i < node.getChildrenCount(); i++)
-					firsts.addAll(GetFirsts(tree, node.getChild(i)));
+					add(firsts, getFirsts(tree, node.getChild(i)));
 			}
 			else if (node.getType() == AstNodeType.CHAR || 
 					 node.getType() == AstNodeType.CHAR_RANGE || 
 					 node.getType() == AstNodeType.NUMBER || 
 					 node.getType() == AstNodeType.STRING ||
 					 node.getType() == AstNodeType.SUB_STAT)
-				firsts.add(node.toString());
+				add(firsts, node.toString());
 			else if (node.getType() == AstNodeType.IDENT) {
 				IAstNode identNode = tree.getNode(node.getName());
 				if (node != null)
-					firsts.addAll(GetFirsts(tree, identNode));
+					add(firsts, getFirsts(tree, identNode));
 			}
 			else if (node.getType() == AstNodeType.NODELIST)
-				firsts.addAll(GetFirsts(tree, node.getChild(0)));
+				add(firsts, getFirsts(tree, node.getChild(0)));
 		}
 		return firsts;
 	}
+	
+	private static void add(Vector<String> firsts, String first) throws Exception {
+		if (firsts.contains(first))
+			throw new Exception(String.format("Found duplicate firsts: %s.", first));
+		else
+			firsts.add(first);
+	}
+	
+	private static void add(Vector<String> firsts1, Vector<String> firsts2) throws Exception {
+		for (String first : firsts2)
+			add(firsts1, first);
+	}
+	
+	/*
+	public static Vector<String> getFollows(IAstTree tree, IAstNode node) {
+		Vector<String> follows = new Vector<String>();
+		if (node.getType() == AstNodeType.IDENT) {
+			IAstNode statListNode = tree.getRoot();
+			for (int i = 0; i < statListNode.getChildrenCount(); i++) {
+				IAstNode statNode = statListNode.getChild(i);
+				IAstNode lhs = statNode.getChild(0);
+				if (!lhs.getName().equals((node.getName()))) {
+					IAstNode rhs = statNode.getChild(1);
+					if (rhs.getType() == AstNodeType.NODELIST) {
+						for (int j = 0; j < rhs.getChildrenCount(); j++) {
+							IAstNode child = rhs.getChild(j);
+							
+						}
+					}
+				}
+			}
+		}
+		return follows;
+	}
+	
+	private static Vector<String> getFollows(IAstNode nodeList, IAstNode node) {
+		Vector<String> follows = new Vector<String>();
+		if (nodeList.getType() == AstNodeType.NODELIST) {
+			for (int i = 0; i < nodeList.getChildrenCount(); i++) {
+				IAstNode child = nodeList.getChild(i);
+				if (child.getName().equals(node.getName())) {
+					if (i < nodeList.getChildrenCount() - 1) {
+						IAstNode follow = nodeList.getChild(i + 1);
+					}
+				}
+			}
+		}
+		return follows;
+	}*/
 }
